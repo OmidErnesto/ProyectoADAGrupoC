85a4b881406d00610860a4d66fd5806e2950a018
OmidErnesto <ochahuaris@unsa.edu.pe>
2021-11-01 17:59:31 -0500

    QuickSort generico implementado para ordenar cualquier dato comparable

---
+++ P1_Ordenamiento/QuickSort/QuickSort.java
@@ -0,0 +1,44 @@
+package QuickSort;
+public class QuickSort<T> {
+
+    public <T extends Comparable<T>> void Qsort(T A[], int izquierda, int derecha) {
+
+        T piv = A[izquierda]; 	  //el pivote sera el primer elemento
+        int i = izquierda;        //busqueda de izquierda a derecha
+        int j = derecha;          //busqueda de derecha a izquierda
+        T aux;
+
+        while(i<j) {                          			//mientras no se crucen                                   
+            while(piv.compareTo(A[i])>=0 && i<j) {
+                i++; 									//busca el elemento mayor que pivote
+            }
+            while(A[j].compareTo(piv)> 0) {
+                j--;           							//busca el elemento menor que pivote
+            }
+            if (i<j) {                        			//si no hay cruce                      
+                aux = A[i];                      		//se intercambia
+                A[i] = A[j];
+                A[j] = aux;
+            }
+        }
+
+        A[izquierda] = A[j];      						//se coloca el pivote en su posicion ordenada                                    
+        A[j] = piv;      								//se coloca menores a su izquierda y mayores a su derecha
+
+        if (izquierda < j-1) {
+            Qsort(A,izquierda,j-1);          		//recursividad para ordenar el subarray izquierdo
+        }
+        if (j+1 < derecha) {
+            Qsort(A,j+1,derecha);          			//recrusividad para order el subarray derecho
+        }
+    }
+    
+    /* Imprimir */
+	public void printArray(T[] arreglo) {
+		int n = arreglo.length;
+		for (int i=0; i<n; i++)
+			System.out.print(arreglo[i] + " ");
+		System.out.println();
+	}
+   
+}
--- P1_Ordenamiento/Test.java
+++ P1_Ordenamiento/Test.java
@@ -1,16 +1,44 @@
 import HeapSort.HeapSort;
+import QuickSort.QuickSort;
 
 public class Test {
 	
 	public static void main(String args[]) {
 		
-        Integer array[] = { 12, 11, 13, 5, 6, 7 };
+        Integer enteros[] = {12,50,99,11,13,78,5,24,66,7,70};
+        String cadenas[] = {"mi","nombre","es","Omid","Chahuaris"};
+        
+      //HeapSort
         
         HeapSort<Integer> ob = new HeapSort<Integer>();
-        ob.sort(array);
+        ob.sort(enteros);
+        HeapSort<String> ob2 = new HeapSort<String>();
+        ob2.sort(cadenas);
  
-        System.out.println("El arreglo ordenado por HeapSort es:");
-        ob.printArray(array);
+        System.out.println("El arreglo de enteros ordenado por HeapSort es:");
+        ob.printArray(enteros);
+        System.out.println("El arreglo de cadenas ordenado por HeapSort es:");
+        ob2.printArray(cadenas);
+        
+        //QuickSort
+        
+        Integer enteros2[] = {12,50,99,11,13,78,5,24,66,7,70};
+        String cadenas2[] = {"mi","nombre","es","Omid","Chahuaris"};
+        
+        QuickSort<Integer> ob3 = new QuickSort<Integer>();
+        ob3.Qsort(enteros2,0,enteros2.length-1);
+        QuickSort<String> ob4 = new QuickSort<String>();
+        ob4.Qsort(cadenas2,0,cadenas2.length-1);
+ 
+        System.out.println("El arreglo de enteros ordenado por QuickSort es:");
+        ob3.printArray(enteros2);
+        System.out.println("El arreglo de cadenas ordenado por QuickSort es:");
+        ob4.printArray(cadenas2);
+        
+        
+        
+        
+     
     }
 
 }
